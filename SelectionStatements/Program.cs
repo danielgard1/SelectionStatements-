9a68693bf9e17cf210b605518d691cb4330d3e9b
danielgard1 <danielgardner1357@gmail.com>
2021-03-14 16:25:31 -0600

    Exercise complete

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 557;
+            
+            Console.WriteLine("Guess my favorite number from 1-1000!");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("Nice Guess, you are correct");
+            }
+        }
+    }
+}
